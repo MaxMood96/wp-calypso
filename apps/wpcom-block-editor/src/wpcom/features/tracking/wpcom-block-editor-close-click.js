f62186ede156408059ee496310327a8c592d7590
David Szabo <david.szabo97@gmail.com>
2021-06-08 15:41:16 +0200

    Track browsing sidebar toggle click

--- apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-close-click.js
+++ apps/wpcom-block-editor/src/wpcom/features/tracking/wpcom-block-editor-close-click.js
@@ -9,7 +9,8 @@ import tracksRecordEvent from './track-record-event';
  * @returns {{handler: Function, selector: string, type: string}} event object definition.
  */
 export default () => ( {
-	selector: '.edit-post-header .edit-post-fullscreen-mode-close',
+	selector:
+		'.edit-post-header .edit-post-fullscreen-mode-close, .edit-site-navigation-toggle__button',
 	type: 'click',
 	handler: () => tracksRecordEvent( 'wpcom_block_editor_close_click' ),
 } );
